a728da20e0ca75dcbdec6fb33678fd126a850169
Jamie Gardner <jgardner05@qub.ac.uk>
2020-01-23 19:45:41 +0000

    MaGi commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
-function setup() {
+let count = 0;
+let o, a;
+let length, angle;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(20);
+  noLoop();
+
+  o = createVector(0, 0);
+  
+  length = 1;
+  angle = 0;
 }
 
 function draw() {
+  stroke(255, 150);
+  translate(windowWidth / 2, windowHeight / 2);
+  
+  while (count < 140) {
+    nextVector();
+    count++;
+  }
+}
 
+function nextVector() {
+  length += 0.1 * length;
+  let a = createVector(1);
+  a.setMag(length);
+  a.rotate(angle);
+  line(o.x, o.y, a.x, a.y);
+  translate(a.x, a.y);
+  angle -= 2.1530;
 }
